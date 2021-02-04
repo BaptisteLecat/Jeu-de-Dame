a07f04751ea38b52f4a1efb5fb6c80b60b0adde1
Baptiste <baptiste.lecat44@gmail.com>
2021-02-05 00:20:08 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,52 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/index.css">
-    <title>plateau</title>
-</head>
+require("board.php");
 
-<body>
-    <div class="container">
-
-
-        <?php
-
-        for ($j = 0; $j < 10; $j++) {
-
-            for ($i = 0; $i < 10; $i++) {
-                if ($j % 2 == 0) {
-                    if ($i % 2 == 0) { ?>
-                        <div class="box-1">
-                            <div class="pawn"></div>
-                        </div>
-                    <?php
-                    } else { ?>
-                        <div class="box-2">
-                            <div class="pawn"></div>
-                        </div>
-                    <?php
-                    }
-                } else {
-                    if ($i % 2 == 0) { ?>
-                        <div class="box-2">
-                            <div class="pawn"></div>
-                        </div>
-                    <?php
-                    } else { ?>
-                        <div class="box-1">
-                            <div class="pawn"></div>
-                        </div>
-        <?php
-                    }
-                }
-            }
-        }
-
-        ?>
-    </div>
-</body>
-
-</html>
+?>
