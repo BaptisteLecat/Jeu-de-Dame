5729b47ed57c7e86504d62f7d1ae0140fe388bdc
Baptiste <baptiste.lecat44@gmail.com>
2021-02-04 21:14:05 +0100

    Creation class Player

--- App/Model/Entity/Player.php
+++ App/Model/Entity/Player.php
@@ -1,8 +1,62 @@
 <?php 
 
-class ClassName extends AnotherClass implements Interface
+class Player implements JsonSerializable
 {
-    
+    private $name;
+    private $score;
+    private $turn;
+
+    public function __construct($name) {
+        $this->name = $name;
+        $this->score = 0;
+        $this->turn = null;
+    }
+
+    public function jsonSerialize()
+    {
+        return array(
+            "name" => $this->name,
+            "score" => $this->score,
+            "turn" => $this->getTurn()
+        );
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    public function getTurn()
+    {
+        if($this->turn === null){
+            throw new Exception("Le turn n'a pas été set");
+            
+        }else{    
+            return $this->turn;
+        }
+    }
+
+    public function addScore(){
+        $this->score++;
+    }
+
+    public function manageTurn($turn = null)
+    {
+        if($turn !== null){
+            if(gettype($turn) == "boolean"){
+                $this->turn = $turn;
+            }else{
+                throw new Exception("Le type passé en paramètre n'est pas un boolean");
+            }
+        }else{
+            $this->turn = ($turn == true) ? false : true;
+        }
+    }
 }
 
 
