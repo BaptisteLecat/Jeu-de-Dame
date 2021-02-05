f7f420e8a431df5f4c5cee49554d4b0b5f25e6c4
Baptiste <baptiste.lecat44@gmail.com>
2021-02-05 18:51:57 +0100

    Correctif et implémentation de l'option de selection

--- public/js/selectBox.js
+++ public/js/selectBox.js
@@ -1,27 +1,19 @@
-/*function getId(Id){
-    alert(Id);
-}
-
-let color = document.getElementById(Id)
-color.addEventListener("click", function(event){
-    parent = color.parentNode;
-    parent.style.backgroundColor = "#4B7980"
-}, false)*/
-
 var selected_pawn = null;
 
 function selectBox(object){
     if(selected_pawn == null){
         selected_pawn = object.id;
+        object.parentNode.classList.add("selected");
     }else{
         //Déselectionné l'element
         var previousPawn = document.getElementById(selected_pawn);
         //Changement de l'etat
-        previousPawn.parentNode.style.backgroundColor = "#4B7980";
+        previousPawn.parentNode.classList.remove("selected");
+        previousPawn.parentNode.classList.add("unselected");
 
         //Set du nouveau pawn
         selected_pawn = object.id;
         //Changement de l'état en selected
-        object.parentNode.style.backgroundColor = "#4B7980";
+        object.parentNode.classList.add("selected");
     }
 }
--- view/board.php
+++ view/board.php
@@ -13,14 +13,24 @@
 
         <?php foreach ($board->getList_Box() as $box) { ?>
             <?php if ($box->getBoxType() == 1) { ?>
-                <div class='box-black' id="<?= $box->getId() ?>">
+                <div class='box-black' name="<?= $box->getId() ?>">
+                    <div class='unselected'>
+                        <div class='pawn' id="<?= $box->getId() ?>" onclick="selectBox(this)">
+                        </div>
+                    </div>
                 </div>
             <?php } else { ?>
-                <div class='box-white' id="<?= $box->getId() ?>">
+                <div class='box-white' name="<?= $box->getId() ?>">
+                    <div class='unselected'>
+                        <div class='pawn' id="<?= $box->getId() ?>" onclick="selectBox(this)">
+                        </div>
+                    </div>
                 </div>
             <?php } ?>
         <?php } ?>
     </div>
+
+    <script src="../public/js/test.js"></script>
 </body>
 
 </html>
